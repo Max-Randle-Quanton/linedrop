31c1e2875115c875e79e292a1fd60d82e2ebd84a
maxrandle <maxrandle95@gmail.com>
2020-12-15 23:42:14 +1300

    get messages resolver

--- server/graphql/resolvers/merge.js
+++ server/graphql/resolvers/merge.js
@@ -1,7 +1,6 @@
 // for merging data types together based on their relationships
 
 const User = require("../../models/user");
-const Group = require("../../models/group");
 const Event = require("../../models/event");
 const { dbDateToString } = require("../../helpers");
 
@@ -20,13 +19,15 @@ const enrichUser = (user) => ({
 
 const enrichGroup = (group) => ({
   ...group._doc,
-  users: () => findUsersByIds(group._doc.users),
-  messages: group._doc.messages.map((message) => ({
-    ...message._doc,
-    author: () => enrichUser(message._doc.author),
-    createdAt: dbDateToString(message._doc.createdAt),
-    updatedAt: dbDateToString(message._doc.updatedAt),
-  })),
+  members: () => findUsersByIds(group._doc.members),
+  messages: group._doc.messages.map((message) => enrichMessage(message)),
+});
+
+const enrichMessage = (message) => ({
+  ...message._doc,
+  author: () => findUserById(message._doc.author),
+  createdAt: dbDateToString(message._doc.createdAt),
+  updatedAt: dbDateToString(message._doc.updatedAt),
 });
 
 const enrichEvent = (event) => ({
@@ -41,13 +42,6 @@ const enrichBooking = (booking) => ({
   event: () => findEventById(booking._doc.event),
   createdAt: dbDateToString(booking._doc.createdAt),
   updatedAt: dbDateToString(booking._doc.updatedAt),
-});
-
-const enrichMessage = (message) => ({
-  ...message._doc,
-  author: () => findUserById(message._doc.author),
-  createdAt: dbDateToString(message._doc.createdAt),
-  updatedAt: dbDateToString(message._doc.updatedAt),
 });
 
 const findUsersByIds = async (userIds) => {
